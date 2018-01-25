75ae6c44b82adebe28d401e2ef9cfd9cf5ba4fd2
Florian Putz <putzflorian@users.noreply.github.com>
2018-01-25 16:32:45 +0100

    Update sheet.php

--- sheet.php
+++ sheet.php
@@ -6,9 +6,6 @@
 $this->get('pimcore.locale')->setLocale('de);
 
 
-// call a service within a template  // sollte nicht verwendet werden besser ist ei view helper
-$productService = $app->getContainer()->get('app.service.product');
-
 // call a service within a action
 $productService = $this->get('app.service.product');
 
